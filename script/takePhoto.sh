43f4610b9ec2555db738f3a173dcd99831574195
yamap55 <tetuya120@hotmail.com>
2018-01-10 01:55:55 +0900

    [fixed] robo message

--- script/takePhoto.sh
+++ script/takePhoto.sh
@@ -6,5 +6,5 @@ basedir=/home/yamap55/raspberrypi-home/script/
 ${basedir}util/jsay "ロボです。写真撮りますよ。3。2。1。"
 filePath=`${basedir}util/cap.py`
 ${basedir}util/jsay "ロボです。写真撮りました。"
-${basedir}util/fileUpload2Slack.py ${filePath} photo ロボが写真撮りました。
+${basedir}util/fileUpload2Slack.py ${filePath} photo ロボガシャシントリマシタ。
 
