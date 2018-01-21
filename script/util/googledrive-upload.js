3d2f8fd27fa4872ecdabc9f56d3d37dbc52eeb61
yamap55 <tetuya120@hotmail.com>
2018-01-22 00:08:16 +0900

    [add] script to upload to google drive

---
+++ script/util/googledrive-upload.js
@@ -0,0 +1,31 @@
+var fs = require('fs');
+var google = require('googleapis');
+var googleAuthWrapper = require("./util/googleDrive.js")
+
+googleAuthWrapper.execute(a);
+
+function a(auth) {
+  var fileMetadata = {
+    'name': 'photo.jpg',
+    parents:[process.env.GOOGLE_DRIVE_FOLDER_ID]
+  };
+  var media = {
+    mimeType: 'image/jpeg',
+    // body: fs.createReadStream('/Users/yamap_55/Desktop/omocha_robot.png')
+    body: fs.createReadStream(process.argv[2])
+  };
+  var service = google.drive('v3');
+  service.files.create({
+    auth: auth,
+    resource: fileMetadata,
+    media: media,
+    fields: 'id'
+  }, function (err, file) {
+    if (err) {
+      // Handle error
+      console.error(err);
+    } else {
+      console.log('File Id: ', file.id);
+    }
+  });
+}
