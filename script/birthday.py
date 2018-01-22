8190569a8f4a9105903dd1ffeabcc73a7cb2be6a
yamap55 <tetuya120@hotmail.com>
2018-01-23 01:29:29 +0900

    [fixed] birth day script

--- script/birthday.py
+++ script/birthday.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python
 # coding: utf-8
 from slacker import Slacker
 import datetime
 from datetime import timedelta
+import os
 
 today = datetime.date.today()
 tyear = today.year
@@ -11,15 +13,18 @@ tmonth = today.month
 token = os.environ["SLACK_TOKEN"]
 slack = Slacker(token)
 channel = "#random"
+#channel = "#test"
 
-with open("../secret/birthday-list.txt", "r") as f:
+with open("/home/yamap55/raspberrypi-home/secret/birthday-list.txt", "r") as f:
     for line in f.readlines():
         # 1987/05/14,今日はhogeの誕生日:{0}歳
         ar = line.replace("\n", "").split(",")
+        if len(ar) < 2:
+            continue
         birthday = ar[0]
         message = ar[1]
         tdatetime = datetime.datetime.strptime(birthday, "%Y/%m/%d")
 
         if tdatetime.day == tday and tdatetime.month == tmonth:
             y = tyear - tdatetime.year
-            slack.chat.post_message(channel, message.format(y))
+            slack.chat.post_message(channel, message.format(y),as_user=True)
