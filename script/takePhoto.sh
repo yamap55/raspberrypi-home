2af2ebd81e440609c430aa5e37257999da14c1f0
yamap55 <tetuya120@hotmail.com>
2020-05-23 21:09:19 +0900

    [add] error handling

--- script/takePhoto.sh
+++ script/takePhoto.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # 写真撮ってSlackにあげるScript
-
 basedir=/home/yamap55/raspberrypi-home/script/
 ${basedir}util/jsay.sh "ロボです。写真撮りますよ。3。2。1。"
-filePath=`${basedir}util/cap.py`
-fileName=`basename ${filePath}`
+filePath=`python3 ${basedir}util/cap.py`
+if [ $? -gt 0 ]; then
+    ${basedir}util/jsay.sh "ロボです。写真が撮れませんでした。パパに連絡してください。"
+    exit 1
+fi
 ${basedir}util/jsay.sh "ロボです。写真撮りました。"
 ${basedir}util/fileUpload2Slack.py ${filePath} photo ${fileName} ロボガシャシントリマシタ。
 node /home/yamap55/raspberrypi-home/googledrive/googledrive-upload.js ${filePath}
-
