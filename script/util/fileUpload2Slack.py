61b8e82eae54748d1b114d4cf4ae6333d88b7624
yamap55 <tetuya120@hotmail.com>
2020-05-24 01:05:04 +0900

    [update] fileUpload2Slack script

--- script/util/fileUpload2Slack.py
+++ script/util/fileUpload2Slack.py
@@ -1,34 +1,36 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+"""
+ファイルをSlackに投稿します
 
-# ファイルをSlackに投げます。
-# メッセージが指定されている場合にはメッセージを投げてからファイルを投げます。
-#
-# 引数
-# filepath, channel, [message]
-#
-# 環境変数「SLACK_TOKEN」が必須
+メッセージが指定されている場合にはファイル投稿前にメッセージが投稿されます
+"""
 
-from slacker import Slacker
-import sys
+import argparse
 import os
+import json
+from slacker import Slacker
 
-# 引数
-# filepath, channel, fileName, [message]
-argvs = sys.argv
-if (len(argvs) < 4):
-    print "args is required."
-    quit()
+SETTING_FILE_PATH = '/home/yamap55/raspberrypi-home/config.json'
 
-filePath = argvs[1]
-channel = "#" + argvs[2]
-fileName = argvs[3]
+def main():
+    parser = argparse.ArgumentParser(description='ファイルをSlackに投稿するスクリプト')
+    parser.add_argument('filePath', help='投稿するファイルPATH')
+    parser.add_argument('channel', help='投稿するチャンネル名')
+    parser.add_argument('-n', '--fileName', help='投稿する際に表示するファイル名')
+    parser.add_argument('-m', '--message', help='ファイル投稿前に投稿するメッセージ')
 
-token = os.environ["SLACK_TOKEN"]
-slack = Slacker(token)
-if (len(argvs) >= 5):
-    message = argvs[4]
-    slack.chat.post_message(channel, message,as_user=True)
+    args = parser.parse_args()
+    filePath = args.filePath
+    channel = args.channel if args.channel[0] == '#' else f'#{args.channel}'
+    fileName = args.fileName if args.fileName else os.path.basename(filePath)
+    message = args.message
 
-slack.files.upload(filePath, filename=fileName, channels=channel)
+    with open(SETTING_FILE_PATH, 'r') as f:
+        config  = json.load(f)
+        slack = Slacker(config['SLACK_TOKEN'])
+        if (args.message):
+            slack.chat.post_message(channel, args.message ,as_user=True)
 
+        slack.files.upload(filePath, filename=fileName, channels=channel)
+
+if __name__=='__main__':
+    main()
